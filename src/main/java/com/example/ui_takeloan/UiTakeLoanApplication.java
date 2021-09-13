c635881f139a0b9a7faa2646762a9bc5f0bd3ce6
adam-p <77075408+adam-pietka@users.noreply.github.com>
2021-09-13 19:51:13 +0200

    corrected

---
+++ src/main/java/com/example/ui_takeloan/UiTakeLoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.ui_takeloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiTakeLoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiTakeLoanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ui_takeloan/UiTakeLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ui_takeloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiTakeLoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
